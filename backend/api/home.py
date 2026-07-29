1cbc1b498d8f29da8204e75dd8447e5f7582f859
mahtabalam139 <156344748+mahtabalam139@users.noreply.github.com>
2026-07-29 10:16:29 +0530

    Update home.py

--- backend/api/home.py
+++ backend/api/home.py
@@ -6,3 +6,11 @@ router = APIRouter()
 @router.get("/")
 def home():
     return RedirectResponse(url="/dashboard")
+
+@router.get("/health")
+def health():
+    return {
+        "application": "Production Monitoring Dashboard",
+        "status": "Running",
+        "version": "1.0.0"
+    }
